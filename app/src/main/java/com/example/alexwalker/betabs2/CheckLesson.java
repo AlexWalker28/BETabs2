2f56bd4a655ebdeb864e29438c15db956446a63c
ThirtySeven <alechka9906@gmail.com>
2016-11-19 21:49:50 +0600

    Rename result to lesson in CheckLesson

--- app/src/main/java/com/example/alexwalker/betabs2/CheckLesson.java
+++ app/src/main/java/com/example/alexwalker/betabs2/CheckLesson.java
@@ -1,9 +1,26 @@
 package com.example.alexwalker.betabs2;
 
 /**
- * Created by AlexWalker on 18.11.2016.
+ * Created by ThirtySeven on 18.11.2016.
  */
 
 public class CheckLesson {
 
+    public static Lesson check(Group group, int lesNum, int lesWeekDay, int lesOrder, boolean isOdd){
+        Lesson lesson = null;
+
+        for (Lesson less : group.getGroupLesson()) {
+            if(lesNum == less.getNumber() &&
+               lesWeekDay == less.getDayOfWeek() &&
+               lesOrder == less.getOrder() &&
+               isOdd == less.getIsOdd()){
+                lesson = less;
+
+
+            }
+            break;
+        }
+        return lesson;
+
+    }
 }
