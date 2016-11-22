680d494204b43a7a77b2b9b864ba1131bd1b211e
AlexWalker <alexwolk28@gmail.com>
2016-11-22 17:56:37 +0600

    Add bones of new adapter

--- app/src/main/java/com/example/alexwalker/betabs2/LessonsListAdapter.java
+++ app/src/main/java/com/example/alexwalker/betabs2/LessonsListAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
@@ -11,144 +12,61 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeSet;
 
-class LessonsListAdapter extends BaseAdapter {
-
-    private static final int TYPE_ITEM = 0;
-    private static final int TYPE_SEPARATOR = 1;
-
-    private ArrayList<String> mData = new ArrayList<String>();
-    private TreeSet<Integer> sectionHeader = new TreeSet<Integer>();
+public class LessonsListAdapter extends ArrayAdapter<Item> {
     private LayoutInflater mInflater;
 
-    public LessonsListAdapter(Context context) {
-        mInflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    public enum RowType {
+        LIST_ITEM, HEADER_ITEM
     }
 
-    public void addItem(final String item) {
-        mData.add(item);
-        notifyDataSetChanged();
-    }
-
-    public void addSectionHeaderItem(final String item) {
-        mData.add(item);
-        sectionHeader.add(mData.size() - 1);
-        notifyDataSetChanged();
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        return sectionHeader.contains(position) ? TYPE_SEPARATOR : TYPE_ITEM;
+    public LessonsListAdapter(Context context, List<Item> items) {
+        super(context, 0, items);
+        mInflater = LayoutInflater.from(context);
     }
 
     @Override
     public int getViewTypeCount() {
-        return 2;
+        return RowType.values().length;
+
     }
 
     @Override
-    public int getCount() {
-        return mData.size();
+    public int getItemViewType(int position) {
+        return getItem(position).getViewType();
     }
 
-    @Override
-    public String getItem(int position) {
-        return mData.get(position);
-    }
+    /*@Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return getItem(position).getView(mInflater, convertView);
+    }*/
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
+    private static final int TYPE_ITEM = 0;
+    private static final int TYPE_SEPARATOR = 1;
 
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder = null;
         int rowType = getItemViewType(position);
+        View View;
         if (convertView == null) {
             holder = new ViewHolder();
             switch (rowType) {
                 case TYPE_ITEM:
                     convertView = mInflater.inflate(R.layout.custom_adapter, null);
-                    holder.textView = (TextView) convertView.findViewById(R.id.lessonName);
+                    holder.View = getItem(position).getView(mInflater, convertView);
                     break;
                 case TYPE_SEPARATOR:
                     convertView = mInflater.inflate(R.layout.custom_adapter_header, null);
-                    holder.textView = (TextView) convertView.findViewById(R.id.textSeparator);
+                    holder.View = getItem(position).getView(mInflater, convertView);
                     break;
             }
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
-        holder.textView.setText(mData.get(position));
-
         return convertView;
     }
 
     public static class ViewHolder {
-        public TextView textView;
+        public View View;
     }
-
 }
-/*
-public class LessonsListAdapter extends BaseAdapter {
-    private List<Lesson> lessonList;
-    private LayoutInflater layoutInflater;
-    Converter converter;
-
-    public LessonsListAdapter(Context context, List<Lesson> lessonList) {
-        this.lessonList = lessonList;
-        layoutInflater = LayoutInflater.from(context);
-    }
-
-    @Override
-    public int getCount() {
-        return lessonList.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return lessonList.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
-        if (convertView == null) {
-            convertView = layoutInflater.inflate(R.layout.custom_adapter, null);
-            holder = new ViewHolder();
-            holder.lessonNumber = (TextView) convertView.findViewById(R.id.lessonNumber);
-            holder.lessonName = (TextView) convertView.findViewById(R.id.lessonName);
-
-
-            convertView.setTag(holder);
-
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-
-
-        holder.lessonNumber.setText(converter.convertLessonNumber(lessonList.get(position).getLessonNumber().getNumber()));
-        holder.lessonName.setText(lessonList.get(position).getLessonName().getFullName());
-        */
-/*holder.lessonOrder.setText(lessonList.get(position).getLessonOrder());*//*
-
-        holder.dayOfTheWeek.setText(converter.convertDay(lessonList.get(position).getWeek().getDayOfTheWeek()));
-       // if (!(position == 0 || lessonList.get(position).getWeek().getDayOfTheWeek() != lessonList.get(position - 1).getWeek().getDayOfTheWeek()))
-        //holder.dayOfTheWeek.setVisibility(View.GONE);
-        return convertView;
-    }
-
-    static class ViewHolder {
-        TextView lessonNumber;
-        TextView lessonName;
-
-        //TextView dayOfTheWeek;
-    }
-
-
-}*/
